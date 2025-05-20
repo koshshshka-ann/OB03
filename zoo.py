f04939b6e25d02cf3c8a851597a4864a611548aa
Anna Siryak <grafova.a.i@gmail.com>
2025-05-20 17:41:47 +0300

    Update zoo.py
    добавила классы животных

--- zoo.py
+++ zoo.py
@@ -8,3 +8,43 @@
 # 4. Используйте композицию для создания класса `Zoo`, который будет содержать информацию о животных и сотрудниках. Должны быть методы для добавления животных и сотрудников в зоопарк.
 
 # 5. Создайте классы для сотрудников, например, `ZooKeeper`, `Veterinarian`, которые могут иметь специфические методы (например, `feed_animal()` для `ZooKeeper` и `heal_animal()` для `Veterinarian`).
+
+# Родительский класс животных
+class Animal():
+    def __init__(self, name, age, food):
+        self.name = name
+        self.age = age
+        self.food = food
+
+    def make_sound(self):
+        pass
+
+    def eat(self):
+        print(f"{self.name} ест {self.food}")
+
+# Класс птиц
+class Bird(Animal):
+    def __init__(self, name, age, food, fly_ability): # спец. атрибут - способность летать
+        super().__init__(name, age, food)
+        self.fly_ability = fly_ability
+
+    def make_sound(self):
+        print(f"{self.name} чирикает: Чик-чирик!")
+
+# Класс млекопитающих
+class Mammal(Animal):
+    def __init__(self, name, age, food, fur_thickness): # спец. атрибут - густота шерсти
+        super().__init__(name, age, food)
+        self.fur_thickness = fur_thickness
+
+    def make_sound(self):
+        print(f"{self.name} рычит: Р-р-р-р-р!")
+
+#Класс пресмыкающихся
+class Reptile(Animal):
+    def __init__(self, name, age, food, body_length): # спец. атрибут - длина тела
+        super().__init__(name, age, food)
+        self.body_length = body_length
+
+    def make_sound(self):
+        print(f"{self.name} шипит: Ш-ш-ш-ш-ш!")
